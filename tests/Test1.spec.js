6c1eff8810884e71b59f8fcdec5285fb6affff4c
Lijo-Hiyield <lijo@hiyield.co.uk>
2025-02-19 11:59:29 +0000

    Type into elements on page

--- tests/Test1.spec.js
+++ tests/Test1.spec.js
@@ -1,19 +1,26 @@
-const {test, expect} = require('@playwright/test')
+    const {test, expect} = require('@playwright/test')
 
-test('browser context test1 code', async ({browser})=>
-{
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-    console.log(await page.title());
+    //test.only
+    test('page test1 code', async ({page})=>
+        {
 
-})
+            await page.goto("https://www.google.co.uk/")
+            await expect(page).toHaveTitle("Google")
 
-//test.only
-test('page test1 code', async ({page})=>
-    {
 
-        await page.goto("https://www.google.co.uk/")
-        await expect(page).toHaveTitle("Google")
+        })
 
-    })
+    test('browser context test1 code', async ({browser})=>
+            {
+                const context = await browser.newContext();
+                const page = await context.newPage();
+                await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+                console.log(await page.title());
+                await page.locator("input#username").fill("rahulshetty")
+                await page.locator("input.form-control").nth(1).fill("learning")
+                await page.waitForTimeout(3000);
+                await page.locator('[name="signin"]').click()
+
+                await page.waitForTimeout(3000);
+            
+            })
