477e6967619995f62983d203b5b94b0b553722de
Lijo-Hiyield <lijo@hiyield.co.uk>
2025-02-19 16:45:51 +0000

    extract element and insert expect assertion

--- tests/Test1.spec.js
+++ tests/Test1.spec.js
@@ -18,8 +18,13 @@
                 console.log(await page.title());
                 await page.locator("input#username").fill("rahulshetty")
                 await page.locator("input.form-control").nth(1).fill("learning")
-                await page.waitForTimeout(3000);
                 await page.locator('[name="signin"]').click()
+
+                await page.waitForTimeout(1000);
+
+                console.log(await page.locator('[style*="block"]').textContent());
+                
+                await expect(page.locator('[style*="block"]')).toContainText('Incorrect');
 
                 await page.waitForTimeout(3000);
             
